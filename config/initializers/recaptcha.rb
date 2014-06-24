dd2b47fcc00400b332b339089cd4002d13d59f3c
hoangphanea <hoang.phan@eastagile.com>
2014-06-24 15:13:39 +0700

    Heroku captcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,5 +1,5 @@
 Recaptcha.configure do |config|
-  config.public_key  = '6LeKvvQSAAAAAAri1dOJ5kwIXk1C1FP-cvLdD2xx'
-  config.private_key = '6LeKvvQSAAAAAJOqxPXMIiIHwXvDYxzO_NKPsnZj'
+  config.public_key  = '6LfJwfUSAAAAAITrOnJBWv6_L2S3OnLkdypKWJfO'
+  config.private_key = '6LfJwfUSAAAAADcWdtO0i5CPxXKXsh_iHIPPxJ9S'
   config.proxy = 'http://www.google.com/recaptcha/api/verify'
 end
